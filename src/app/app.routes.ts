8d0c277d97d06540da73786ff41a44e5cf883f47
erkinov26 <erkinov0247@gmail.com>
2025-07-02 19:48:24 +0500

    changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { PATHS } from '../core/navigation';
+import { PATHS } from '../core/navigation/navigation';
 
 export const routes: Routes = [
   {
